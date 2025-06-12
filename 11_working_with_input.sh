00381405d112c078e835f1741c74d9a5d8b5d20e
utsav <utshavshrestha.1713@gmail.com>
2025-06-12 22:11:45 +0545

    some changes

--- 11_working_with_input.sh
+++ 11_working_with_input.sh
@@ -5,12 +5,16 @@ read name
 
 echo "my name is $name"
 
+# Give prompt insed the read use -p 
+
 read -p "where do you live " address
 echo "I live at $address"
 
+# Silent input use -sp
 read -sp "Enter your password " password
 echo -e "\npassword save"
 
+#for the multiple input 
 echo "Enter your first name and last name"
 read firstname lastname
 echo "my full name is $firstname $lastname"
