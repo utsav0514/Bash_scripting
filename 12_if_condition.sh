3db1fb54775e5fd2589733b1e9605865d5432392
utsav <utshavshrestha.1713@gmail.com>
2025-06-12 22:21:31 +0545

    about if condition

---
+++ 12_if_condition.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Enter your age " Age
+
+if [ $Age -gt 20 ]; then
+	echo "you are able to drink alcohol"
+
+elif [ $Age -lt 10 ]; then
+	echo "you are only allowed to drink milk"
+
+else
+	echo "you are only allowed to dring colddrink"
+
+fi
